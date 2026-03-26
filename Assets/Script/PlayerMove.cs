dda80faa0f7480e2a6d7fb14efd52ecd7b990874
yoodohyun03 <yoodohyun03@gmail.com>
2026-03-26 22:00:03 +0900

    캐릭터 뛰기 기능 추가(미완성)

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using Photon.Pun;
-using Unity.Cinemachine; // ★ 네임스페이스가 이렇게 바뀌었습니다!
+using Unity.Cinemachine;
 
 public class PlayerMove : MonoBehaviourPun
 {
     public float speed = 5f;
 
+    // ★ 애니메이터를 조종하기 위한 변수
+    private Animator anim;
+
     void Start()
     {
+        // ★ 내 캐릭터의 애니메이터 부품을 찾아서 변수에 쏙 넣습니다.
+        anim = GetComponent<Animator>();
+
         if (photonView.IsMine)
         {
-            // ★ 클래스 이름도 CinemachineCamera로 짧아졌습니다!
             CinemachineCamera vcam = FindObjectOfType<CinemachineCamera>();
-
             if (vcam != null)
             {
                 vcam.Follow = this.transform;
@@ -25,10 +29,22 @@ public class PlayerMove : MonoBehaviourPun
     {
         if (!photonView.IsMine) return;
 
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
         Vector3 moveDir = new Vector3(h, 0, v).normalized;
-        transform.Translate(moveDir * speed * Time.deltaTime, Space.World);
+
+        if (moveDir.magnitude > 0)
+        {
+            transform.Translate(moveDir * speed * Time.deltaTime, Space.World);
+            // ★ 요기! 이동 중일 때는 무조건 MoveSpeed를 1로 고정해서 덜덜거림 방지
+            anim.SetFloat("MoveSpeed", 1.0f);
+        }
+        else
+        {
+            // ★ 요기! 완전히 멈췄을 때만 0으로 바꿈
+            anim.SetFloat("MoveSpeed", 0f);
+        }
+
+        // 마우스 회전 코드는 그대로 두시고요!
     }
 }
